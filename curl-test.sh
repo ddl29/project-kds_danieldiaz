9ebf0a3a4de3367ab49264ab383d3ca778b8684b
DanielDiaz <danieldiazlopez29@gmail.com>
2022-06-24 00:29:45 -0500

    Bash script to test GET, POST and DELETE

--- curl-test.sh
+++ curl-test.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
 
 NAME=$(echo $RANDOM | md5sum | head -c 6)
-EMAIL=$()
 
 echo "TESTING POST"
 curl --request POST http://localhost:5000/api/timeline_post -d 'name='$NAME'&email='$NAME'@net.com&content=Testing '$NAME''
