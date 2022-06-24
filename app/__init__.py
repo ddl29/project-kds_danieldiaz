22493ddcb9f1af0ff5d6413dc098c76e415e0e43
DanielDiaz <danieldiazlopez29@gmail.com>
2022-06-24 00:25:55 -0500

    Adding MySQL and GET/POST endpoints

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,22 @@
+from email.policy import default
 import os
+from pyexpat import model
 from flask import Flask, render_template, request
 from dotenv import load_dotenv
+from peewee import *
+import datetime
+from playhouse.shortcuts import model_to_dict
 
 load_dotenv()
 app = Flask(__name__)
+
+mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"),
+        user=os.getenv("MYSQL_USER"),
+        password=os.getenv("MYSQL_PASSWORD"),
+        host=os.getenv("MYSQL_HOST"),
+        port=3306
+)
+print(mydb)
 
 
 @app.route('/')
@@ -42,7 +55,46 @@ def chuu_education():
 def diaz():
     return render_template('diaz.html', title="Daniel Diaz Portfolio")
 
+@app.route('/api/timeline_post', methods=['POST'])
+def post_time_line_post():
+    name = request.form['name']
+    email = request.form['email']
+    content = request.form['content']
+    timeline_post = TimelinePost.create(name=name, email=email, content=content)
 
+    return model_to_dict(timeline_post)
 
+@app.route('/api/timeline_post', methods=['GET'])
+def get_time_line_post():
+    return {
+        'timeline_posts':[
+            model_to_dict(p)
+            for p in TimelinePost.select().order_by(TimelinePost.created_at.desc())
+        ]
+    }
 
+@app.route('/api/timeline_post', methods=['DELETE'])
+def delete_time_line_post():
+    name = request.form['name']
+    
+    matches = {
+        'timeline_posts':[
+            model_to_dict(p)
+            for p in TimelinePost.select().where(TimelinePost.name==name)
+        ]
+    }
+    TimelinePost.delete().where(TimelinePost.name==name).execute()
+    return matches
 
+#Database creation and connection
+class TimelinePost(Model):
+    name = CharField()
+    email = CharField()
+    content = CharField()
+    created_at = DateTimeField(default=datetime.datetime.now)
+
+    class Meta:
+        database = mydb
+
+mydb.connect()
+mydb.create_tables([TimelinePost])
